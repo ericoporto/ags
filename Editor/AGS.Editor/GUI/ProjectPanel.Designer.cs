1d828780bbcdd1d80e12421e842302c677c7c870
e <ericoporto2008@gmail.com>
2021-09-19 23:13:54 -0300

    Sketch Editor: search in project explorer

--- Editor/AGS.Editor/GUI/ProjectPanel.Designer.cs
+++ Editor/AGS.Editor/GUI/ProjectPanel.Designer.cs
@@ -30,6 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProjectPanel));
             this.projectTree = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.textBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // projectTree
@@ -39,21 +47,70 @@
             this.projectTree.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.projectTree.HideSelection = false;
             this.projectTree.LabelEdit = true;
-            this.projectTree.Location = new System.Drawing.Point(1, 20);
+            this.projectTree.Location = new System.Drawing.Point(0, 0);
             this.projectTree.Name = "projectTree";
-            this.projectTree.Size = new System.Drawing.Size(290, 239);
+            this.projectTree.Size = new System.Drawing.Size(290, 210);
             this.projectTree.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(1, 20);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.projectTree);
+            this.splitContainer1.Size = new System.Drawing.Size(290, 239);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Controls.Add(this.button1);
+            this.AcceptButton = this.button1;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Image = global::AGS.Editor.Properties.Resources.eye;
+            this.button1.Location = new System.Drawing.Point(261, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 18);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ProjectPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 260);
-            this.Controls.Add(this.projectTree);
+            this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ProjectPanel";
             this.Padding = new System.Windows.Forms.Padding(1, 20, 1, 1);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.textBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -61,5 +118,8 @@
         #endregion
 
         internal System.Windows.Forms.TreeView projectTree;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Editor/AGS.Editor/GUI/ProjectPanel.cs
+++ Editor/AGS.Editor/GUI/ProjectPanel.cs
@@ -24,5 +24,57 @@ namespace AGS.Editor
             projectTree.ForeColor = t.GetColor("project-panel/project-tree/foreground");
             projectTree.LineColor = t.GetColor("project-panel/project-tree/line");
         }
+
+        private bool ContainsTextAtRight(string text, char sep, string search)
+        {
+            string[] split_txt = text?.Split(sep);
+            if(split_txt.Length > 1)
+            {
+                string txt_to_search = split_txt[1].Trim();
+                return txt_to_search == search;
+            }
+            return false;
+        }
+
+        private TreeNode SearchTreeView(string p_sSearchTerm, TreeNodeCollection p_Nodes)
+        {
+            foreach (TreeNode node in p_Nodes)
+            {
+                if (node.Text == p_sSearchTerm || ContainsTextAtRight(node.Text, ':', p_sSearchTerm))
+                {
+                    return node;
+                }
+
+                if (node.Nodes.Count > 0)
+                {
+                    var result = SearchTreeView(p_sSearchTerm, node.Nodes);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                TreeNode resultTreeNode = SearchTreeView(textBox1.Text, projectTree.Nodes);
+                if (resultTreeNode != null)
+                {
+                    projectTree.SelectedNode = resultTreeNode;
+                    resultTreeNode?.Parent?.Expand();
+                    resultTreeNode?.Parent?.Parent?.Expand();
+                    resultTreeNode.TreeView.Focus();
+                }
+            }
+        }
     }
 }
