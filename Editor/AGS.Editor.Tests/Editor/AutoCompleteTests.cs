e29711ad7ab11466b1a4027e3daa2619c196c928
e <ericoporto2008@gmail.com>
2022-02-21 22:18:52 -0300

    test example

---
+++ Editor/AGS.Editor.Tests/Editor/AutoCompleteTests.cs
@@ -0,0 +1,32 @@
+﻿using AGS.Types;
+using NUnit.Framework;
+
+namespace AGS.Editor
+{
+    [TestFixture]
+    public class AutoCompleteTests
+    {
+        private static Script CachedAutoCompletedScriptFromCode(string scriptCode)
+        {
+            Script script = new Script("_test", scriptCode, false);
+            AutoComplete.ConstructCache(script);
+            return script;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void CheckAutoCompleteVariables()
+        {
+            string scriptCode = $@"
+int a;
+";
+            Script scriptToTest = CachedAutoCompletedScriptFromCode(scriptCode);
+
+            Assert.That(scriptToTest.AutoCompleteData.Variables.Count, Is.EqualTo(1));            
+        }
+    }
+}
