42e2637fe91ea633ffeb71df326723f709848c5a
e <ericoporto2008@gmail.com>
2022-05-29 11:27:18 -0300

    Editor: new Log panel to read game log when debugging

---
+++ Editor/AGS.Editor/GUI/LogPanel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace AGS.Editor
+{
+    public partial class LogPanel : DockContent
+    {
+        public LogPanel()
+        {
+            InitializeComponent();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.logTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.logTextBox.Text += text;
+                Show();
+            }
+        }
+        public void WriteLogMessage(string message, string groupID, string groupName, string mtID, string mt)
+        {
+            SetText("[" + groupName + "][" + mt + "]: " + message + "\n");
+        }
+    }
+}
