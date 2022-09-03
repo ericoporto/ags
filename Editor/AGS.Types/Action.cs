725efc7c8cc398d450d8735d4059e36294dc85a8
e <ericoporto2008@gmail.com>
2022-09-03 13:59:06 -0300

    Editor: action component initial sketch

---
+++ Editor/AGS.Types/Action.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AGS.Types
+{
+    public class Action : IToXml, IComparable<Action>
+    {
+        private string _name;
+        private int _id;
+        private string _triggerEventHandler = string.Empty;
+
+        public Action()
+        {
+            _name = string.Empty;
+        }
+
+        [Description("Script function to run when the action is triggered")]
+        [Category("Events")]
+        [Browsable(false)]
+        [AGSEventProperty()]
+        [ScriptFunctionParameters("Action *action, bool state")]
+        [EditorAttribute(typeof(ScriptFunctionUIEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        public string OnTrigger
+        {
+            get { return _triggerEventHandler; }
+            set { _triggerEventHandler = value; }
+        }
+
+        [Description("The ID number of the action")]
+        [Category("Design")]
+        [ReadOnly(true)]
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [Description("The script name of the action")]
+        [Category("Design")]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = Utilities.ValidateScriptName(value); }
+        }
+
+        #region IComparable
+        public int CompareTo(Action other)
+        {
+            return ID.CompareTo(other.ID);
+        }
+        #endregion IComparable
+
+        public Action(XmlNode node)
+        {
+            SerializeUtils.DeserializeFromXML(this, node);
+        }
+
+        #region IToXml
+        public void ToXml(XmlTextWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion IToXml
+    }
+}
---
+++ Editor/AGS.Types/ActionFolder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace AGS.Types
+{
+    public class ActionFolders : FolderListHybrid<Action, ActionFolder>
+    {
+        public ActionFolders() : base(new ActionFolder()) { }
+
+        public ActionFolders(string name) : base(new ActionFolder(name)) { }
+
+        public ActionFolders(XmlNode node, XmlNode parentNodeForBackwardsCompatability) :
+            base(new ActionFolder(node, parentNodeForBackwardsCompatability))
+        { }
+    }
+
+
+    public class ActionFolder : BaseFolderCollection<Action, ActionFolder>
+    {
+        public const string MAIN_ACTION_FOLDER_NAME = "Main";
+
+        public ActionFolder(string name) : base(name) { }
+
+        public ActionFolder() : this("Default") { }
+
+        public ActionFolder(XmlNode node, XmlNode parentNodeForBackwardsCompatability) :
+            base(node, parentNodeForBackwardsCompatability)
+        { }
+
+        private ActionFolder(XmlNode node) : base(node) { }
+
+        public override ActionFolder CreateChildFolder(string name)
+        {
+            return new ActionFolder(name);
+        }
+
+        public Action FindActionByID(int actionID, bool recursive)
+        {
+            return FindItem(IsItem, actionID, recursive);
+        }
+
+        protected override ActionFolder CreateFolder(XmlNode node)
+        {
+            return new ActionFolder(node);
+        }
+
+        protected override Action CreateItem(XmlNode node)
+        {
+            return new Action(node);
+        }
+
+        private bool IsItem(Action action, int actionID)
+        {
+            return action.ID == actionID;
+        }
+
+    }
+}
