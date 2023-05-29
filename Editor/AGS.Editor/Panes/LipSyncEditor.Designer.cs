8c85e0185f6022679aa34203934e860b8ba378b2
e <ericoporto2008@gmail.com>
2023-05-29 19:53:18 -0300

    Editor: fix LipSync Editor scaling for 100% and 200%

--- Editor/AGS.Editor/Panes/LipSyncEditor.Designer.cs
+++ Editor/AGS.Editor/Panes/LipSyncEditor.Designer.cs
@@ -28,48 +28,75 @@ namespace AGS.Editor
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelLipSyncIntro1 = new System.Windows.Forms.Label();
+            this.labelLipSyncIntro2 = new System.Windows.Forms.Label();
+            this.panelLipSyncList = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // labelLipSyncIntro1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Use the property grid on the right to enable/disable lip sync.";
+            this.labelLipSyncIntro1.AutoSize = true;
+            this.labelLipSyncIntro1.Location = new System.Drawing.Point(4, 4);
+            this.labelLipSyncIntro1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelLipSyncIntro1.Name = "labelLipSyncIntro1";
+            this.labelLipSyncIntro1.Size = new System.Drawing.Size(373, 17);
+            this.labelLipSyncIntro1.TabIndex = 0;
+            this.labelLipSyncIntro1.Text = "Use the property grid on the right to enable/disable lip sync.";
             // 
-            // label2
+            // labelLipSyncIntro2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 30);
-            this.label2.MaximumSize = new System.Drawing.Size(400, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(367, 26);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "In the text boxes, type the letters that will cause that frame to be shown. Separ" +
+            this.labelLipSyncIntro2.AutoSize = true;
+            this.labelLipSyncIntro2.Location = new System.Drawing.Point(4, 29);
+            this.labelLipSyncIntro2.Margin = new System.Windows.Forms.Padding(4);
+            this.labelLipSyncIntro2.MaximumSize = new System.Drawing.Size(400, 0);
+            this.labelLipSyncIntro2.Name = "labelLipSyncIntro2";
+            this.labelLipSyncIntro2.Size = new System.Drawing.Size(392, 34);
+            this.labelLipSyncIntro2.TabIndex = 1;
+            this.labelLipSyncIntro2.Text = "In the text boxes, type the letters that will cause that frame to be shown. Separ" +
     "ate multiple letters with slashes, eg.   C/D/G/K";
+            // 
+            // panelLipSyncList
+            // 
+            this.panelLipSyncList.Location = new System.Drawing.Point(3, 70);
+            this.panelLipSyncList.Name = "panelLipSyncList";
+            this.panelLipSyncList.Size = new System.Drawing.Size(565, 424);
+            this.panelLipSyncList.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.labelLipSyncIntro1);
+            this.flowLayoutPanel1.Controls.Add(this.labelLipSyncIntro2);
+            this.flowLayoutPanel1.Controls.Add(this.panelLipSyncList);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(661, 548);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.Layout += new System.Windows.Forms.LayoutEventHandler(this.flowLayoutPanel1_Layout);
             // 
             // LipSyncEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "LipSyncEditor";
-            this.Size = new System.Drawing.Size(661, 369);
+            this.Size = new System.Drawing.Size(661, 548);
             this.Load += new System.EventHandler(this.LipSyncEditor_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelLipSyncIntro1;
+        private System.Windows.Forms.Label labelLipSyncIntro2;
+        private System.Windows.Forms.Panel panelLipSyncList;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
