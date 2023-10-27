d587ba23d9b281a178c774fa59c7ba0eafb7adba
e <ericoporto2008@gmail.com>
2023-10-27 10:38:47 -0300

    Engine: use extern C in main_sdl2
    The application's main() function must be called with C linkage, according to `SDL_main.h`.

--- Engine/main/main_sdl2.cpp
+++ Engine/main/main_sdl2.cpp
@@ -1,6 +1,15 @@
 #include <SDL.h>
 #include "main/main.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 int main(int argc, char *argv[]) { 
     return ags_entry_point(argc, argv);
 }
+
+#ifdef __cplusplus
+} // END of Extern "C
+#endif
