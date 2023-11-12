809c0c9c519447cb004a82a39031bfe183934b97
e <ericoporto2008@gmail.com>
2023-11-12 16:55:48 -0300

    Editor: add rooms overview

---
+++ Editor/AGS.Editor/GUI/RoomsOverview.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace AGS.Editor.GUI
+{
+    partial class RoomsOverview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 450);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // RoomsOverview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "RoomsOverview";
+            this.Text = "RoomsOverview";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ Editor/AGS.Editor/GUI/RoomsOverview.cs
@@ -0,0 +1,89 @@
+﻿using AGS.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AGS.Editor.GUI
+{
+    public partial class RoomsOverview : Form
+    {
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(System.Drawing.Drawing2D.WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void saveThumb(Room room)
+        {
+            int t_w = 256;
+            int t_h = 256;
+            using (Bitmap b = new Bitmap(t_w, t_h))
+            {
+                using (Graphics g = Graphics.FromImage(b))
+                {
+                    IntPtr hdc = g.GetHdc();
+                    int r_w = room.Width;
+                    int r_h = room.Height;
+
+                    float w_scale = ((float)t_w) / ((float)r_w);
+                    float h_scale = ((float)t_h) / ((float)r_h);
+                    float scale = Math.Max(w_scale, h_scale);
+
+                    int off_x = (int)((t_w - (r_w * scale)) / 2);
+                    int off_y = (int)((t_h - (r_h * scale)) / 2);
+
+                    Factory.NativeProxy.DrawRoomBackground(hdc, room, off_x, off_y, 0, scale, RoomAreaMaskType.None,
+                        0, 0);
+                    Factory.NativeProxy.RenderBufferToHDC(hdc);
+                    g.ReleaseHdc(hdc);
+                }
+                Bitmap thumb = ResizeImage(b, 96, 96);
+                thumb.Save(room.FileName.Replace(".crm",".png"), ImageFormat.Png);
+            }
+        }
+        public RoomsOverview()
+        {
+            InitializeComponent();
+
+            foreach (var ri in Factory.AGSEditor.CurrentGame.Rooms)
+            {
+                UnloadedRoom unloaded_room = new UnloadedRoom(ri.Number);
+                Room room = Factory.NativeProxy.LoadRoom(unloaded_room);
+                saveThumb(room);
+            }
+        }
+    }
+}
