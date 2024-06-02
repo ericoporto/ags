fd8e95ca5d6ca2ae8be9075969dc2e8f191ad555
e <ericoporto2008@gmail.com>
2024-06-02 12:03:10 -0300

    Engine: ScriptSprintf can work with any size
    use internal ScriptSprintfImpl that can take a null and calculate necessary length

--- Engine/script/script_api.cpp
+++ Engine/script/script_api.cpp
@@ -63,47 +63,29 @@ inline const char *GetArgPtr(const RuntimeScriptValue *sc_args, va_list *varg_pt
 }
 
 
-// TODO: this implementation can be further optimised by either not calling
-// snprintf but formatting values ourselves, or by using some library method
-// that supports customizing, such as getting arguments in a custom way.
-const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
-                          const RuntimeScriptValue *sc_args, int32_t sc_argc, va_list *varg_ptr)
-{
-    if (!buffer || buf_length == 0)
-    {
-        cc_error("Internal error in ScriptSprintf: buffer is null");
-        return "";
-    }
-    if (!format)
-    {// NOTE: interpreter (usually) catches null-pointer sent as format at some stage earlier
-        cc_error("Internal error in ScriptSprintf: format string is null");
-        return "";
-    }
-    if (!varg_ptr && sc_argc > 0 && !sc_args)
-    {
-        cc_error("Internal error in ScriptSprintf: args pointer is null");
-        return "";
-    }
-
-    // Expected format character count:
-    // percent sign:    1
-    // flag:            1
-    // field width      10 (an uint32 number)
-    // precision sign   1
-    // precision        10 (an uint32 number)
-    // length modifier  2
-    // type             1
-    // NOTE: although width and precision will
-    // not likely be defined by a 10-digit
-    // number, such case is theoretically valid.
+int ScriptSprintfImpl(char *buffer, size_t buf_length, const char *format, const RuntimeScriptValue *sc_args, int32_t sc_argc, va_list *varg_ptr) {
+// Expected format character count:
+// percent sign:    1
+// flag:            1
+// field width      10 (an uint32 number)
+// precision sign   1
+// precision        10 (an uint32 number)
+// length modifier  2
+// type             1
+// NOTE: although width and precision will
+// not likely be defined by a 10-digit
+// number, such case is theoretically valid.
     const size_t fmtbuf_size = 27;
     char       fmtbuf[fmtbuf_size];
     char       *fmt_bufptr;
     char       *fmt_bufendptr = &fmtbuf[fmtbuf_size - 1];
 
-    char       *out_ptr    = buffer;
+    size_t total_length = 0;
+    bool calculate_length_only = (buffer == nullptr);
+    char dummy_buffer[1] = {0};
+    char       *out_ptr    = calculate_length_only ? dummy_buffer : buffer;
     // save 1 character for null terminator
-    const char *out_endptr = buffer + buf_length - 1;
+    const char *out_endptr = calculate_length_only ? dummy_buffer : (buffer + buf_length - 1);
     const char *fmt_ptr    = format;
     int32_t    arg_idx     = 0;
 
@@ -112,7 +94,7 @@ const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
     FormatParseResult fmt_done;
 
     // Parse the format string, looking for argument placeholders
-    while (*fmt_ptr && out_ptr != out_endptr)
+    while (*fmt_ptr && (calculate_length_only || out_ptr != out_endptr))
     {
         // Try to put argument into placeholder
         if (*fmt_ptr == '%')
@@ -177,7 +159,11 @@ const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
             if (fmt_done == kFormatParseLiteralPercent)
             {
                 // literal percent sign
-                *(out_ptr++) = '%';
+                if (!calculate_length_only)
+                {
+                    *(out_ptr++) = '%';
+                }
+                total_length++;
                 continue;
             }
             else if (fmt_done >= kFormatParseArgFirst && fmt_done <= kFormatParseArgLast &&
@@ -190,15 +176,15 @@ const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
                 switch (fmt_done)
                 {
                 case kFormatParseArgInteger:
-                    snprintf_res = snprintf(out_ptr, avail_outbuf + 1, fmtbuf, GetArgInt(sc_args, varg_ptr, arg_idx)); break;
+                    snprintf_res = snprintf(calculate_length_only ? nullptr : out_ptr, calculate_length_only ? 0 : (avail_outbuf + 1), fmtbuf, GetArgInt(sc_args, varg_ptr, arg_idx)); break;
                 case kFormatParseArgFloat:
-                    snprintf_res = snprintf(out_ptr, avail_outbuf + 1, fmtbuf, GetArgFloat(sc_args, varg_ptr, arg_idx)); break;
+                    snprintf_res = snprintf(calculate_length_only ? nullptr : out_ptr, calculate_length_only ? 0 : (avail_outbuf + 1), fmtbuf, GetArgFloat(sc_args, varg_ptr, arg_idx)); break;
                 case kFormatParseArgCharacter:
                 {
                     int chr = GetArgInt(sc_args, varg_ptr, arg_idx);
                     char cbuf[5]{};
                     usetc(cbuf, chr);
-                    snprintf_res = snprintf(out_ptr, avail_outbuf + 1, "%s", cbuf);
+                    snprintf_res = snprintf(calculate_length_only ? nullptr : out_ptr, calculate_length_only ? 0 : (avail_outbuf + 1), "%s", cbuf);
                     break;
                 }
                 case kFormatParseArgString:
@@ -215,46 +201,100 @@ const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
                         else
                         {
                             cc_error("!ScriptSprintf: formatting argument %d is expected to be a string, but it is a null pointer", arg_idx + 1);
-                            return "";
+                            return -1;
                         }
                     }
                     else if (fmt_done == kFormatParseArgString && p == buffer)
                     {
                         cc_error("!ScriptSprintf: formatting argument %d is a pointer to output buffer", arg_idx + 1);
-                        return "";
+                        return -1;
                     }
-                    snprintf_res = snprintf(out_ptr, avail_outbuf + 1, fmtbuf, p);
+                    snprintf_res = snprintf(calculate_length_only ? nullptr : out_ptr, calculate_length_only ? 0 : (avail_outbuf + 1), fmtbuf, p);
                     break;
                 }
                 case kFormatParseArgPointer:
-                    snprintf_res = snprintf(out_ptr, avail_outbuf + 1, fmtbuf, GetArgPtr(sc_args, varg_ptr, arg_idx)); break;
-                default: /* should not happen */ break;
+                    snprintf_res = snprintf(calculate_length_only ? nullptr : out_ptr, calculate_length_only ? 0 : (avail_outbuf + 1), fmtbuf, GetArgPtr(sc_args, varg_ptr, arg_idx)); break;
+                    default: /* should not happen */ break;
                 }
 
                 arg_idx++;
                 if (snprintf_res >= 0)
                 {
-                    // snprintf returns maximal number of characters, so limit it with buffer size
-                    out_ptr += std::min<ptrdiff_t>(snprintf_res, avail_outbuf);
+                    int added_length = snprintf_res;
+                    if(!calculate_length_only)
+                    {
+                        // snprintf returns maximal number of characters, so limit it with buffer size
+                        added_length = std::min<ptrdiff_t>(added_length, avail_outbuf);
+                        out_ptr += added_length;
+                    }
+                    total_length += added_length;
                     continue;
                 }
                 // -- pass further to invalid format case
             }
-            
+
             // If format was not valid, or there are no available
             // parameters, just copy stored format buffer as it is
-            size_t copy_len = std::min(std::min<ptrdiff_t>(fmt_bufptr - fmtbuf, fmtbuf_size - 1), avail_outbuf);
-            memcpy(out_ptr, fmtbuf, copy_len);
-            out_ptr += copy_len;
+            ptrdiff_t remaining_size_arg = std::min<ptrdiff_t>(fmt_bufptr - fmtbuf, fmtbuf_size - 1);
+            size_t copy_len = static_cast<size_t>(remaining_size_arg);
+            if (!calculate_length_only)
+            {
+                copy_len = std::min(remaining_size_arg, avail_outbuf);
+                memcpy(out_ptr, fmtbuf, copy_len);
+                out_ptr += copy_len;
+            }
+            total_length += copy_len;
         }
         // If there's no placeholder, simply copy the character to output buffer
         else
         {
-            *(out_ptr++) = *(fmt_ptr++);
+            if (!calculate_length_only)
+            {
+                *(out_ptr++) = *(fmt_ptr++);
+            }
+            else
+            {
+                fmt_ptr++;
+            }
+            total_length++;
         }
     }
-
+    total_length++; // space for null character
     // Terminate the string
-    *out_ptr = 0;
-    return buffer;
+    if (!calculate_length_only) {
+        *out_ptr = 0;
+    }
+    return total_length;
+}
+
+// TODO: this implementation can be further optimised by either not calling
+// snprintf but formatting values ourselves, or by using some library method
+// that supports customizing, such as getting arguments in a custom way.
+const char *ScriptSprintf(char *buffer, size_t buf_length, const char *format,
+                          const RuntimeScriptValue *sc_args, int32_t sc_argc, va_list *varg_ptr)
+{
+    // Fix-me: Not sure what to do here yet, perhaps moving the internals to an impl and leaving this on outside?
+    if (!buffer || buf_length == 0)
+    {
+        cc_error("Internal error in ScriptSprintf: buffer is null");
+        return "";
+    }
+    if (!format)
+    {// NOTE: interpreter (usually) catches null-pointer sent as format at some stage earlier
+        cc_error("Internal error in ScriptSprintf: format string is null");
+        return "";
+    }
+    if (!varg_ptr && sc_argc > 0 && !sc_args)
+    {
+        cc_error("Internal error in ScriptSprintf: args pointer is null");
+        return "";
+    }
+    int buffer_length = ScriptSprintfImpl(nullptr, 0, format, sc_args, sc_argc, varg_ptr);
+    if(buffer_length < 0) {
+        return "";
+    }
+
+    char *final_buffer = new char[buffer_length];
+    ScriptSprintfImpl(final_buffer, buffer_length, format, sc_args, sc_argc, varg_ptr);
+    return final_buffer;
 }
