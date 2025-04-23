3c4ae09fa54068b46572dcb4dfd45937cdfde988
Érico Porto <ericoporto2008@gmail.com>
2025-04-23 11:47:43 -0300

    CMake: googletest fetch redone for future proofness
    Googletest fetching in our CMake is sufficiently dettached it can be redone to support MakeAvailable without much effort.

--- CMake/FetchGoogleTest.cmake
+++ CMake/FetchGoogleTest.cmake
@@ -1,49 +1,40 @@
-if(LINUX AND CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0)
-    set(LINUX_OLD_GCC TRUE)
+set(LINUX_OLD_GCC FALSE)
+if(UNIX AND (CMAKE_CXX_COMPILER_ID STREQUAL "GNU") AND (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0))
+        set(LINUX_OLD_GCC TRUE)
+endif()
+
+if(LINUX_OLD_GCC)
     FetchContent_Declare(
-        googletest_content
-        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-        URL https://github.com/google/googletest/archive/refs/tags/release-1.8.1.tar.gz
-        URL_HASH MD5=2e6fbeb6a91310a16efe181886c59596
+            googletest
+            URL https://github.com/google/googletest/archive/refs/tags/release-1.8.1.tar.gz
+            URL_HASH MD5=2e6fbeb6a91310a16efe181886c59596
     )
 else()
-    set(LINUX_OLD_GCC FALSE)
     FetchContent_Declare(
-        googletest_content
-        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-        URL https://github.com/google/googletest/archive/52204f78f94d7512df1f0f3bea1d47437a2c3a58.tar.gz
-        URL_HASH MD5=9512a106bb006ab84e0a822ec363c6c7
+            googletest
+            URL https://github.com/google/googletest/archive/52204f78f94d7512df1f0f3bea1d47437a2c3a58.tar.gz
+            URL_HASH MD5=9512a106bb006ab84e0a822ec363c6c7
+            DOWNLOAD_EXTRACT_TIMESTAMP true
     )
 endif()
 
-message("googletest building with LINUX_OLD_GCC:" ${LINUX_OLD_GCC})
-if(LINUX_OLD_GCC)
-    get_property(
-            compile_options
-            DIRECTORY
-            PROPERTY COMPILE_OPTIONS
-    )
+message(STATUS "googletest building with LINUX_OLD_GCC: ${LINUX_OLD_GCC}")
 
-    set_property(
-            DIRECTORY
-            APPEND
-            PROPERTY COMPILE_OPTIONS -Wno-undef -Wno-missing-noreturn -Wno-inline
-    )
-endif()
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-FetchContent_GetProperties(googletest_content)
-if(NOT googletest_content_POPULATED)
-    FetchContent_Populate(googletest_content)
-    # For Windows: Prevent overriding the parent project's compiler/linker settings
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-    add_subdirectory(${googletest_content_SOURCE_DIR} ${googletest_content_BINARY_DIR} EXCLUDE_FROM_ALL)
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24")
+    FetchContent_MakeAvailable(googletest)
+else()
+    FetchContent_GetProperties(googletest)
+    if(NOT googletest_POPULATED)
+        FetchContent_Populate(googletest)
+
+        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
 endif()
 
 if(LINUX_OLD_GCC)
-    set_property(
-            DIRECTORY
-            PROPERTY COMPILE_OPTIONS ${compile_options}
-    )
-
-    unset(compile_options)
+    target_compile_options(gtest PRIVATE -Wno-undef -Wno-missing-noreturn -Wno-inline)
+    target_compile_options(gtest_main PRIVATE -Wno-undef -Wno-missing-noreturn -Wno-inline)
 endif()
