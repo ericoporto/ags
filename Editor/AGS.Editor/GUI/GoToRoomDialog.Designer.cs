78fb529e8a63da94b3c4044dad5c1523d04c76b4
Érico Porto <ericoporto2008@gmail.com>
2025-08-11 13:05:41 -0300

    Editor: add Go To Room by Number dialog

---
+++ Editor/AGS.Editor/GUI/GoToRoomDialog.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AGS.Editor
+{
+    partial class GoToRoomDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRoomNumber = new System.Windows.Forms.Label();
+            this.upDownRoomNumber = new System.Windows.Forms.NumericUpDown();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.upDownRoomNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLineNumber
+            // 
+            this.lblRoomNumber.AutoSize = true;
+            this.lblRoomNumber.Location = new System.Drawing.Point(15, 11);
+            this.lblRoomNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRoomNumber.Name = "lblLineNumber";
+            this.lblRoomNumber.Size = new System.Drawing.Size(174, 16);
+            this.lblRoomNumber.TabIndex = 0;
+            this.lblRoomNumber.Text = "Select or type room number:";
+            // 
+            // upDownLineNumber
+            // 
+            this.upDownRoomNumber.Location = new System.Drawing.Point(19, 42);
+            this.upDownRoomNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.upDownRoomNumber.Name = "upDownLineNumber";
+            this.upDownRoomNumber.Size = new System.Drawing.Size(249, 22);
+            this.upDownRoomNumber.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(72, 115);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(94, 29);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "&OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(174, 115);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 29);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // GoToRoomDialog
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(282, 157);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.upDownRoomNumber);
+            this.Controls.Add(this.lblRoomNumber);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GoToRoomDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Go To Room";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.upDownRoomNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRoomNumber;
+        private System.Windows.Forms.NumericUpDown upDownRoomNumber;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Editor/AGS.Editor/GUI/GoToRoomDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AGS.Editor
+{
+    public partial class GoToRoomDialog : Form
+    {
+        public GoToRoomDialog()
+        {
+            InitializeComponent();
+            (this.upDownRoomNumber.Controls[1] as TextBox).Enter += upDownLineNumber_Controls1_Enter;
+        }
+
+        private delegate void Action();
+
+        private void upDownLineNumber_Controls1_Enter(object sender, EventArgs e)
+        {
+            BeginInvoke((Action)(() =>
+            {
+                (this.upDownRoomNumber.Controls[1] as TextBox).SelectAll();
+            }));
+        }
+
+        public int RoomNumber
+        {
+            get { return (int)upDownRoomNumber.Value; }
+            set { upDownRoomNumber.Value = value; }
+        }
+
+        public int Minimum
+        {
+            get { return (int)upDownRoomNumber.Minimum; }
+            set 
+            { 
+                upDownRoomNumber.Minimum = value;
+                SetLabelText();
+            }
+        }
+
+        public int Maximum
+        {
+            get { return (int)upDownRoomNumber.Maximum; }
+            set
+            {
+                upDownRoomNumber.Maximum = value;
+                SetLabelText();
+            }
+        }
+
+        private void SetLabelText()
+        {
+            lblRoomNumber.Text = String.Format("Room Number ({0} - {1}):", Minimum, Maximum);
+        }
+    }
+}
